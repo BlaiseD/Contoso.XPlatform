e429657e23415ab376e2f9ac034df03572d6ee17
Blaise Taylor <btaylor@bPolicySolutions.com>
2021-08-26 08:03:22 -0400

    LB libraries 4.1.3-preview.1.1

--- Contoso.AutoMapperProfiles/ExpansionDescriptorToOperatorMappingProfile.cs
+++ Contoso.AutoMapperProfiles/ExpansionDescriptorToOperatorMappingProfile.cs
@@ -10,12 +10,9 @@ namespace Contoso.AutoMapperProfiles
         public ExpansionDescriptorToOperatorMappingProfile()
         {
             CreateMap<SelectExpandDefinitionDescriptor, SelectExpandDefinition>();
-            CreateMap<SelectExpandItemFilterDescriptor, SelectExpandItemFilter>()
-                .ForMember(dest => dest.FilterBody, opts => opts.Ignore())
-                .ForMember(dest => dest.ParameterName, opts => opts.Ignore());
+            CreateMap<SelectExpandItemFilterDescriptor, SelectExpandItemFilter>();
             CreateMap<SelectExpandItemDescriptor, SelectExpandItem>();
-            CreateMap<SelectExpandItemQueryFunctionDescriptor, SelectExpandItemQueryFunction>()
-                .ForMember(dest => dest.MethodCallDescriptor, opts => opts.Ignore());
+            CreateMap<SelectExpandItemQueryFunctionDescriptor, SelectExpandItemQueryFunction>();
             CreateMap<SortCollectionDescriptor, SortCollection>()
                 .ForMember(dest => dest.Skip, opts => opts.MapFrom(src => src.Skip.HasValue ? src.Skip.Value : 0))
                 .ForMember(dest => dest.Take, opts => opts.MapFrom(src => src.Take.HasValue ? src.Take.Value : int.MaxValue));
